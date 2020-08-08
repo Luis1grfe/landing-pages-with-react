98fad2394777b84906ec537ccae1f7da89eb1e56
Elvira Quiroz <63673743+ElviraQDP@users.noreply.github.com>
2020-08-08 19:39:47 -0400

    Update index.js
    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom, { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Navbar from './navbar';
+import Jumpbrotom from './jumpbrotom'
+import Card from './card'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+    return (
+        <div className="container">
+            <Navbar title="Start BootStrap" />
+
+            <Jumpbrotom title="A Warm Welcome!" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat." buttonLabel="Call to action!" />
+
+            <div className="row text-centered">
+
+                <Card imageURl="http://placehold.it/500x325" title="Card title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque." buttonLabel="Find out More!" />
+
+                <Card imageURl="http://placehold.it/500x325" title="Card title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque." buttonLabel="Find out More!" />
+
+                <Card imageURl="http://placehold.it/500x325" title="Card title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque." buttonLabel="Find out More!" />
+
+                <Card imageURl="http://placehold.it/500x325" title="Card title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque." buttonLabel="Find out More!" />
+
+            </div>
+        </div>
+    )
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
+
+
+
