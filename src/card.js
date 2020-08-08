ebbbbefa4484bc469b091546538c01b2bf9ac2e8
Elvira Quiroz <63673743+ElviraQDP@users.noreply.github.com>
2020-08-08 19:43:22 -0400

    Update card.js
    ready

--- src/card.js
+++ src/card.js
@@ -1,1 +1,34 @@
 
+import React from "react";
+import PropTypes from "prop-types";
+
+const Card = props => {
+    return (
+
+
+        <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+                <img className="card-img-top" src={props.imageURl} alt="" />
+                <div className="card-body">
+                    <h4 className="card-title">{props.title}</h4>
+                    <p className="card-text">{props.description}</p>
+                </div>
+                <div className="card-footer">
+                    <a href="#" className="btn btn-primary">{props.buttonLabel}</a>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+
+
+Card.propTypes = {
+    imageURl: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    buttonLabel: PropTypes.string,
+}
+
+export default Card;
